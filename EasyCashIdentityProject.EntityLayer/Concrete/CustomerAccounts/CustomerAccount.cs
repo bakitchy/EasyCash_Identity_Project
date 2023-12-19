00ac7e1a75818df5c143417278544cbd211c279a
bakitchy <bakitchy@gmail.com>
2023-12-19 14:45:59 +0300

    ...

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccounts/CustomerAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete.CustomerAccounts
+{
+    /*müşterinin banka hesap bilgilerini tutar*/
+    public class CustomerAccount
+    {
+        public int CustomerAccountId { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string CustomerAccountBankBranch { get; set; }
+     
+     
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccounts/CustomerAccountProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete.CustomerAccounts
+{
+    /*müşterinin hesap hareketlerini tutar*/
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string CustomerAccountProcessType { get; set; }
+        public decimal CustomerAccountProcessAmount { get; set; }
+        public DateTime CustomerAccountProcessDate { get; set; }
+    }
+}
